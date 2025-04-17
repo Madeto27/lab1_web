8fa0be6d0cc25f391d845510621427f984031047
nazikus <nitronazar@gmail.com>
2025-04-17 18:35:58 +0300

    Method renamed and reset() added.

--- lab1_web/src/test/java/antoniuk/nazar/AutoparkTest.java
+++ lab1_web/src/test/java/antoniuk/nazar/AutoparkTest.java
@@ -2,22 +2,28 @@ package antoniuk.nazar;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class AutoparkTest {
+    @BeforeEach
+    public void resetAutopark() {
+        Autopark.reset(); // Clears all cars, resets state
+    }
+
     @Test
-    public void addVehicleTest(){
+    public void parkVehicleTest(){
         Autopark autopark = Autopark.getAutopark(10, "Maidan St.");
         Auto auto = new Auto("Dodge", 10);
-        autopark.addVehicle(auto);
-        assertTrue(autopark.getCars().contains(auto));
+        autopark.parkVehicle(auto);
+        assertTrue(autopark.getParkedAutos().contains(auto));
     }
 
     @Test
     public void filledNormalTest(){
         Autopark autopark = Autopark.getAutopark(10, "Maidan St.");
         Auto auto = new Auto("Dodge", 10);
-        autopark.addVehicle(auto);
+        autopark.parkVehicle(auto);
         assertEquals(0.1, autopark.getFilled());
     }
 
@@ -26,8 +32,8 @@ public class AutoparkTest {
         Autopark autopark = Autopark.getAutopark(1, "Maidan St.");
         Auto auto1 = new Auto("Dodge", 10);
         Auto auto2 = new Auto("Dodge", 10);
-        autopark.addVehicle(auto1);
-        autopark.addVehicle(auto2);
+        autopark.parkVehicle(auto1);
+        autopark.parkVehicle(auto2);
         assertEquals(1, autopark.getFilled());
     }
 }
